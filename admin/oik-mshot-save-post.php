134e55984a214c1a029f5e007b4de76c958f4951
Herb Miller <herb@bobbingwide.com>
2016-12-15 08:38:22 +0000

    Issue #2 - first pass implementation of 'save_post' hook

---
+++ admin/oik-mshot-save-post.php
@@ -0,0 +1,66 @@
+<?php // (C) Copyright Bobbing Wide 2016
+
+/**
+ * Lazy implementation of save_post for oik-mshot
+ * 
+ * @param ID $post_id
+ * @param object $post
+ */
+function oikms_lazy_save_post( $post_id, $post ) {
+	$fields = oikms_get_mshot_fields( $post->post_type );
+	if ( count( $fields ) ) {
+		foreach ( $fields as $field ) {
+			oikms_check_mshot_id( $post_id, $post, $field );
+		}
+	}
+}
+
+/**
+ * Return array of mshot2 fields
+ * 
+ * mshot2 fields contain cached mshots stored as attachments
+ * 
+ * The url part contains the address
+ * The id is the post ID of the saved screenshot
+ * 
+ * @param string $post_type
+ * @return array of mshot2 field names
+ */
+function oikms_get_mshot_fields( $post_type ) {
+	$fields = array();
+	global $bw_mapping;
+	if ( isset(  $bw_mapping['field'][$post_type] )) {
+		foreach ( $bw_mapping['field'][$post_type] as $field ) {
+			$type = bw_query_field_type( $field );
+			if ( "mshot2" == $type ) {
+				$fields[ $field ] = $field;
+			}
+		}
+	}	
+	bw_trace2( $fields, "fields", false );
+	return( $fields );
+}
+
+/**
+ * Check if the current mshot ID needs updating
+ * 
+ * @TODO 
+ * - Obtain the current serialized value for the given post and field name
+ * - Check if we need to do anything
+ * - Set the $_POST[ $field ] to the serialized value to be saved
+ * 
+ * @param ID $post_id
+ * @param object $post
+ * @param string $field - the field name
+ */
+function oikms_check_mshot_id( $post_id, $post, $field ) {
+	//gob();
+	$mshot = $_POST[ $field ];
+	$id = 19851;
+	// We need to store it as a serialized field, not an array
+	// 
+	$_POST[ $field ] = serialize( array( "url" => $mshot, "id" => $id ) );
+	
+	bw_trace2( $_POST, "_POST" );
+
+}
